c532593a32d0adb75bf384b0cb69b7c8d9b0539c
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-20 20:20:18 -0300

    criação do componente MovieCard , usado na listagem de filmes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
 
 const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
@@ -24,7 +25,7 @@ const Home = () => {
         <h2 className="title">Melhores filmes:</h2>
         <div className="movie-container">
             {topMovies.length === 0 && <p>Carregando...</p>}
-            {topMovies.length > 0 && topMovies.map((movie) => <p key={movie.id}>{movie.title}</p>)}
+            {topMovies.length > 0 && topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
         </div>
     </div>)
 };
