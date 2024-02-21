3db7d968b78000094a79ea2e33aa777ce39fe70e
vinimanfrin <viniciusmanfrin2019@gmail.com>
2024-02-21 18:27:37 -0300

    implementação e estilização da funcionalidade de detalhar um filme

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,5 +1,61 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import {
+    BsGraphUp,
+    BsWallet2,
+    BsHourglassSplit,
+    BsFillFileEarmarkTextFill
+} from "react-icons/bs";
+
+import MovieCard from "../components/MovieCard";
+
+import "./Movie.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Movie = () => {
-    return <h1>Movie</h1>
+    const {id} = useParams()
+    const [movie,setMovie] = useState(null)
+
+    const getMovie = async (url) => {
+        const res = await fetch(url);
+        const data = await res.json()
+
+        setMovie(data);
+    }
+
+    useEffect(()=> {
+        const movieUrl = `${moviesURL}${id}?${apiKey}`
+        getMovie(movieUrl);
+    },[])
+
+    return (
+        <div className="container">
+            {movie && 
+                <div className="movie">
+                    
+                    <div>
+                        <MovieCard movie={movie} showLink={false}/>
+                    </div>
+
+                    <div>
+                        <h3>
+                            <BsHourglassSplit /> Duração:
+                        </h3>
+                        <p>{movie.runtime} minutos</p>
+                    </div>
+
+                    <div>
+                        <h3>
+                            <BsFillFileEarmarkTextFill /> Descrição:
+                        </h3>
+                        <p>{movie.overview} minutos</p>
+                    </div>
+                </div> 
+            }
+        </div>
+    )
 }
 
 export default Movie
